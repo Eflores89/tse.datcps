1ff7dccf303ccd4b452d4d4ad96302e4176905ec
Eduardo Flores <eduardo@enelmargen.org>
2017-01-30 15:58:58 -0600

    init commit
    only 7 states

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' KML's con Indicadores por Codigo Postal
+#'
+#' Una lista de KML's manipulados en R, con indicadores preliminares, para usarse en Mapas de Google.
+#'
+#' @format Una lista con SpatialPolygonDataFrames, uno por cada estado.
+#' @source \url{http://catalogo.datos.gob.mx/dataset/ubicacion-de-codigos-postales-en-mexico}
+"codigospostales"
